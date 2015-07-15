b76fbb347570d81f0065a1e2c835988ff18f5f16
cigreja <cigreja@live.com>
2015-07-15 17:26:36 -0400

    ICheckListAction initial comments

--- src/main/java/com/elle/analyster/presentation/filter/ICheckListAction.java
+++ src/main/java/com/elle/analyster/presentation/filter/ICheckListAction.java
@@ -3,11 +3,21 @@ package com.elle.analyster.presentation.filter;
 import java.util.ArrayList;
 import java.util.Collection;
 
-
+/**
+ * 
+ * This is used as a type in ActionCheckListModel.
+ * The nested class CheckAll also implements this interface.
+ * @param <T> 
+ */
 public interface ICheckListAction<T> {
     
     void check(ICheckListModel<T> model, boolean value);
     
+    /**
+     * CLASS CheckAll
+     * 1 usage from ActionCheckListModel
+     * @param <T> 
+     */
     public static class CheckAll<T> implements ICheckListAction<T> {
 
         @Override
