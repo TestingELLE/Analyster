6de7a3cb80d0f6d9100e1334d305958a647710ab
cigreja <cigreja@live.com>
2015-07-15 17:19:08 -0400

    IListFilter initial comments

--- src/main/java/com/elle/analyster/presentation/filter/IListFilter.java
+++ src/main/java/com/elle/analyster/presentation/filter/IListFilter.java
@@ -1,5 +1,9 @@
 package com.elle.analyster.presentation.filter;
 
+/**
+ * INTERFACE IListFilter
+ * This is implemented in CheckListFilterType
+ */
 public interface IListFilter {
     public boolean include(String element, String pattern);
 }
