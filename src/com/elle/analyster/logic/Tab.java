e9203fe38908999a4ded84b387b22290e6867579
cigreja <cigreja@live.com>
2015-09-25 08:14:17 -0400

    - add switch button to each tab to set individual states for each tab (Tab class variable: boolean switchBtnEnabled)

--- src/com/elle/analyster/logic/Tab.java
+++ src/com/elle/analyster/logic/Tab.java
@@ -36,6 +36,7 @@ public class Tab implements ITableConstants{
     private boolean archiveRecordMenuItemEnabled;  // enables archive record menu item
     private boolean addRecordsBtnVisible;          // sets the add records button visible
     private boolean batchEditBtnVisible;           // sets the batch edit button visible
+    private boolean switchBtnEnabled;              // sets the switch button enabled
     
     // each tab can either be editing or not
     private boolean Editing;           
