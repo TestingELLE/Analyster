a6b2ac6d107a2addaf8f63c47e49c260e5286576
cigreja <cigreja@live.com>
2016-01-28 11:24:39 -0500

    new jar 1.0.2

--- src/com/elle/analyster/presentation/AnalysterWindow.java
+++ src/com/elle/analyster/presentation/AnalysterWindow.java
@@ -55,8 +55,8 @@ import java.util.Vector;
 public class AnalysterWindow extends JFrame implements ITableConstants {
 
     // Edit the version and date it was created for new archives and jars
-    private final String CREATION_DATE = "2016-1-26";
-    private final String VERSION = "1.0.1";
+    private final String CREATION_DATE = "2016-1-28";
+    private final String VERSION = "1.0.2";
 
     // attributes
     private Map<String, Tab> tabs; // stores individual tab objects 
