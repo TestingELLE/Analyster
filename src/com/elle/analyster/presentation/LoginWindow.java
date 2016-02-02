12a095a8fc44d231abf21e132ff7c2c0cf856a3e
cigreja <cigreja@live.com>
2016-02-02 10:45:03 -0500

    removed unused imports

--- src/com/elle/analyster/presentation/LoginWindow.java
+++ src/com/elle/analyster/presentation/LoginWindow.java
@@ -13,7 +13,6 @@ import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
