ae33a99cbc04b3d920e1da981ecfd27e87afc362
cigreja <cigreja@live.com>
2016-02-17 20:33:53 -0500

    New Jar 1.0.6a Access level defaults to user

--- src/com/elle/analyster/presentation/AnalysterWindow.java
+++ src/com/elle/analyster/presentation/AnalysterWindow.java
@@ -68,8 +68,8 @@ import java.util.Vector;
 public class AnalysterWindow extends JFrame implements ITableConstants {
 
     // Edit the version and date it was created for new archives and jars
-    private final String CREATION_DATE = "2016-2-8";
-    private final String VERSION = "1.0.5";
+    private final String CREATION_DATE = "2016-2-17";
+    private final String VERSION = "1.0.6a";
 
     // attributes
     private Map<String, Tab> tabs; // stores individual tab objects 
