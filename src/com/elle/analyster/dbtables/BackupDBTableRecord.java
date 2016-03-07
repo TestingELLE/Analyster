e171f8c414891520e8c60dd82f492861ebaf3a67
cigreja <cigreja@live.com>
2016-03-07 18:01:24 -0500

    added BackupDBTableRecord and BackupDBTableDAO

---
+++ src/com/elle/analyster/dbtables/BackupDBTableRecord.java
@@ -0,0 +1,50 @@
+
+package com.elle.analyster.dbtables;
+
+/**
+ * BackupDBTableRecord
+ * @author Carlos Igreja
+ * @since  Mar 7, 2016
+ */
+public class BackupDBTableRecord {
+
+    private String id;
+    private String tableName;
+    private String backupTableName;
+
+    public BackupDBTableRecord(String id, String tableName, String backupTableName) {
+        this.id = id;
+        this.tableName = tableName;
+        this.backupTableName = backupTableName;
+    }
+    
+    public BackupDBTableRecord(){
+        this(null,null,null);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getBackupTableName() {
+        return backupTableName;
+    }
+
+    public void setBackupTableName(String backupTableName) {
+        this.backupTableName = backupTableName;
+    }
+    
+    
+}
